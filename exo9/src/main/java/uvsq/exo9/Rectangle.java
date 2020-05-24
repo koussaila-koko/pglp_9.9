a37025578d280ada70778b6a2dc46f104d2abf06
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-24 23:42:47 +0200

    Create Rectangle.java

---
+++ exo9/src/main/java/uvsq/exo9/Rectangle.java
@@ -0,0 +1,92 @@
+package uvsq.exo9;
+
+import uvsq.exo9.Forme;
+
+/**
+ * class Rectangle.
+ * 
+ * @author koussaila HAMMOUCHE.
+ *
+ */
+
+public class Rectangle extends Forme {
+
+	/**
+	 *  point pour creer un rectangle.
+	 */
+	private Point point;
+
+	/**
+	 *  longueur du rectangle.
+	 */
+	private double longueur;
+
+	/**
+	 * argeur du rectangle.
+	 */
+	private double largeur;
+
+	/**
+	 * constructeur Rectangle.
+	 * 
+	 * @param name     du rectangle.
+	 * @param p        point pour creer un rectangle.
+	 * @param lon      longueur du rectangle.
+	 * @param lar      largeur du rectangle.
+	 */
+	public Rectangle(final String name, final Point p,
+			final double lon, final double lar) {
+		super(name);
+		this.point = p;
+		this.longueur = lon;
+		this.largeur = lar;
+	}
+
+	/**
+	 * retourner le point.
+	 * 
+	 * @return p.
+	 */
+	public Point getP() {
+		return point;
+	}
+
+	/**
+	 * retoune la longueur du rectangle.
+	 * 
+	 * @return longueur.
+	 */
+	public double getLongueur() {
+		return longueur;
+	}
+
+	/**
+	 * retourne la largeur du rectangle.
+	 * 
+	 * @return largeur.
+	 */
+	public double getLargeur() {
+		return largeur;
+	}
+
+
+	/**
+	 * print le rectangle
+	  */
+	public void print() {
+		System.out.println("Rectangle{" + "nom='" + this.getName() + '\'' + ", p1=" + point.toString() + ", longueur="
+				+ longueur + ", largeur=" + largeur + "}\n");
+
+	}
+	/**
+	 * deplacer le rectangle
+	 * @param x .
+	 * @param y .
+	 */
+	@Override
+	public void deplacer(final int x, final int y) {
+		this.point.sommePoint(x, y);
+
+	}
+
+}
