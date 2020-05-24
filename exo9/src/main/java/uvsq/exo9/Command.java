3d007b4373cca7191a12b2de6058255880ed0713
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-24 23:42:09 +0200

    Create Command.java

---
+++ exo9/src/main/java/uvsq/exo9/Command.java
@@ -0,0 +1,23 @@
+package uvsq.exo9;
+
+/**
+ * interface Command .
+ * 
+ * @author Koussaila HAMMOUCHE.
+ */
+public interface Command {
+	/**
+	 * init .
+	 * @param s .
+	 */
+	void init(final String S);
+	/**
+	 * execute
+	 */
+	void execute();
+	/**
+	 * print
+	 */
+	void print();
+
+}
