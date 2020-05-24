751127b3dd1df94c49e7d7891b8642490ec9b97b
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-24 23:41:48 +0200

    Create CommandMoveCercle.java

---
+++ exo9/src/main/java/uvsq/exo9/CommandMoveCercle.java
@@ -0,0 +1,46 @@
+package uvsq.exo9;
+
+/**
+ * class CommandMoveCercle.
+ * 
+ * @author Koussaila HAMMOUCHE.
+ *
+ */
+
+public class CommandMoveCercle implements CommandMove {
+	/**
+	 * un cercle.
+	 */
+	private Cercle cercle;
+
+	/**
+	 * deplacemnt sur l'axe x.
+	 */
+	private int depX;
+
+	/**
+	 * deplacement sur l'axe y.
+	 */
+	private int depY;
+
+	/**
+	 * constructeur CommandMoveCercle.
+	 * 
+	 * @param c cercle.
+	 * @param dx deplacement sur l'axe x.
+	 * @param dy deplacement sur l'axe y.
+	 */
+	public CommandMoveCercle(final Cercle c, final int dx, final int dy) {
+		this.cercle = c;
+		this.depX = dx;
+		this.depY = dy;
+	}
+
+	@Override
+	/**
+	 * executer la commande du deplacement pour un cercle.
+	 */
+	public void execute() {
+		this.cercle.deplacer(depX, depY);
+	}
+}
