a31fd19893bdec861acdecd3426a133ef919b95d
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-24 23:41:29 +0200

    Create Dao.java

---
+++ exo9/src/main/java/uvsq/exo9/Dao.java
@@ -0,0 +1,42 @@
+package uvsq.exo9;
+
+/**
+ * interface Dao.
+ * 
+ * @author koussaila HAMMOUCHE.
+ *
+ * @param <T> object.
+ */
+
+public interface Dao<T> {
+	/**
+	 * les operations .
+	 *
+	 * @param obj object.
+	 * @return object .
+	 */
+
+	T create( final T obj);
+	/**
+	 * find .
+	 *
+	 * @param obj object.
+	 * @return object .
+	 */
+	T find(final String s);
+	/**
+	 * update .
+	 *
+	 * @param obj object.
+	 * @return object .
+	 */
+	T update(final T obj);
+	/**
+	 * delete supp.
+	 *
+	 * @param obj object.
+	.
+	 */
+	void delete(final T obj);
+
+}
