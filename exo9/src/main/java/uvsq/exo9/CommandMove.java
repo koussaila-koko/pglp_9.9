932aedf05c8388af7b467e40b0c1d0f802681399
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-24 23:43:32 +0200

    Create CommandMove.java

---
+++ exo9/src/main/java/uvsq/exo9/CommandMove.java
@@ -0,0 +1,15 @@
+package uvsq.exo9;
+
+/**
+ * CommandMove.
+ * 
+ * @author Koussaila HAMMOUCHE.
+ *
+ */
+public interface CommandMove extends Icommande {
+	/**
+	 * executer les commande move 
+	 * pour se  deplacer.
+	 */
+	void execute();
+}
