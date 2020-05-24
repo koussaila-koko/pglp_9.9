67e22c860ca89c44e3a7e16692038672052b1d82
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-24 23:43:42 +0200

    Create CommandMoveCarre.java

---
+++ exo9/src/main/java/uvsq/exo9/CommandMoveCarre.java
@@ -0,0 +1,47 @@
+package uvsq.exo9;
+
+/**
+ * class CommandMoveCarre.
+ * 
+ * @author Koussaila HAMMOUCHE.
+ *
+ */
+public class CommandMoveCarre implements CommandMove {
+
+	/**
+	 * un carre.
+	 */
+	private Carre carre;
+
+	/**
+	 *  deplacement sur l'axe x.
+	 */
+	private int depX;
+
+	/**
+	 * deplacement sur l'axe y.
+	 */
+	private int depY;
+
+	/**
+	 * constructeur CommandMoveCarre.
+	 * 
+	 * @param c carre.
+	 * @param dx deplacement sur l'axe x.
+	 * @param dy deplacement sur l'axe y.
+	 */
+	public CommandMoveCarre(final Carre c, final int dx,
+			final int dy) {
+		this.carre = c;
+		this.depX = dx;
+		this.depY = dy;
+	}
+
+	/**
+	 * executer la commande pour deplacer un carre.
+	 */
+	public void execute() {
+		this.carre.deplacer(depX, depY);
+	}
+
+}
